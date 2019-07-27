e9d763e0362f9624a2ef970ec6ebc7f35f5529c6
Sudarshan Dilip Gundale <53350481+5udarshan@users.noreply.github.com>
2019-07-27 22:08:44 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,6 @@
+c=input("Enter any alphabet:-")
+if ((c=='a')|(c=='e')|(c=='i')|(c=='o')|(c=='u')):
+    print("The given alphabet is vowel",c)
+else:
+    print("The given alphabet is consonant",c)
+input("enter to exit")
